86e68fbe95025a0d9bded7f88c8feeeae2ce1906
LucurisClassic <54478419+LucurisClassic@users.noreply.github.com>
2019-11-12 14:22:00 -0800

    Update Kazzak.py

--- NPCscan_Alert/Kazzak.py
+++ NPCscan_Alert/Kazzak.py
@@ -6,7 +6,7 @@ client = discord.Client()
 def discordReport():
 	yield from client.wait_until_ready()
 	counter = 0
-	channel = discord.Object(id='349500178971099136')
+	channel = discord.Object(id='') #YOUR DESIRED CHANNEL ID!!!!!!!!!!!!!!!!!!!!
 	alertMSG = '@everyone Lord Kazzak SPAWN - Blasted Lands ! :Kazzak:'
 
 	while True:
@@ -16,12 +16,6 @@ def discordReport():
 			counter += 1
 		else:
 			pass
-	   
-	# while counter <=3:
-	# 	yield from client.send_message(channel, alertMSG)
-	# 	yield from asyncio.sleep(1) # task runs every 60 seconds
-	# 	counter += 1
-
 @client.async_event
 def on_ready():
 	print('Logged in as')
@@ -30,4 +24,4 @@ def on_ready():
 	print('------')
 
 client.loop.create_task(discordReport())
-client.run('MzQ5NDgzMTQ4NTE5NjY5NzYy.DH2PJw.Hulyyag963N87Nqc3t6envSSF4w')
+client.run('')# YOUR BOT'S TOKEN HERE
