fc0331bc2471f688cd800748f4af6323f0f347d9
LucurisClassic <54478419+LucurisClassic@users.noreply.github.com>
2019-11-15 09:57:33 -0800

    Update Scout.py to new syntax

--- Scout.py
+++ Scout.py
@@ -3,23 +3,23 @@ import sys
 import time
 import datetime
 import subprocess
-screenshotsfolder = 'C:\\Program Files (x86)\\World of Warcraft_classic_\\Screenshots'
+screenshotsfolder = 'C:\\Program Files (x86)\\World of Warcraft\\_classic_\\Screenshots'
 def eventTrigger(unixtimestamp,filename):
-	print ("Detected file %s @ %s" % (os.path.join(screenshotsfolder,"ScreenShot_"+filename+".jpeg"), unixtimestamp))
+	print ("Detected file %s @ %s" % (os.path.join(screenshotsfolder,"WoWScrnShot_"+filename+".jpg"), unixtimestamp))
 	if sys.argv[1] == '-Hinterlands':
-		filePicker = 'C:\\Users\\lucuris\\Downloads\\NPCscan_Alert\\Hinterlands.py'
+		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Hinterlands.py'
 	elif sys.argv[1] == '-Ashenvale':
-		filePicker = 'C:\\Users\\lucuris\\Downloads\\NPCscan_Alert\\Ashenvale.py'
+		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Ashenvale.py'
 	elif sys.argv[1] == '-Feralas':
-		filePicker = 'C:\\Users\\lucuris\\Downloads\\NPCscan_Alert\\Feralas.py'
+		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Feralas.py'
 	elif sys.argv[1] == '-Duskwood':
-		filePicker = 'C:\\Users\\lucuris\\Downloads\\NPCscan_Alert\\Duskwood.py'
+		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Duskwood.py'
 	elif sys.argv[1] == '-Kazzak':
-		filePicker = 'C:\\Users\\lucuris\\Downloads\\NPCscan_Alert\\Kazzak.py'
+		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Kazzak.py'
 	elif sys.argv[1] == '-Azuregos':
-		filePicker = 'C:\\Users\\lucuris\\Downloads\\NPCscan_Alert\\Azuregos.py'
+		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Azuregos.py'
 	else:
-		filePicker = 'C:\\Users\\lucuris\\Downloads\\NPCscan_Alert\\noZoneSpecified.py' 	
+		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\noZoneSpecified.py' 	
 	cmd = ['python3',filePicker]
 	subprocess.Popen(cmd)
 
@@ -29,8 +29,8 @@ def activeScanner():
 	try:
 		while True:
 			for file in os.listdir(screenshotsfolder):
-				if file.startswith("ScreenShot_") and file.endswith(".jpeg"):
-					fndtstr =  file.replace("ScreenShot_",'').replace(".jpeg",'')
+				if file.startswith("WoWScrnShot_") and file.endswith(".jpg"):
+					fndtstr =  file.replace("WoWScrnShot_",'').replace(".jpg",'')
 					fndt = time.mktime(datetime.datetime.strptime(fndtstr, "%m%d%y_%H%M%S").timetuple())
 					if fndt not in data.keys():
 						if fndt > first_ran_time:
@@ -48,11 +48,3 @@ def main():
 	activeScanner()
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
-
-
