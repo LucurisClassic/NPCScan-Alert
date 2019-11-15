b697fa46da751d848fee46d3b5a2f0f2d7e15cf1
LucurisClassic <54478419+LucurisClassic@users.noreply.github.com>
2019-11-15 09:58:52 -0800

    Updated Kazzak.py to new syntax

--- Kazzak.py
+++ Kazzak.py
@@ -2,21 +2,17 @@ import sys
 import discord
 import asyncio
 client = discord.Client()
-@asyncio.coroutine
-def discordReport():
-	yield from client.wait_until_ready()
-	counter = 0
-	channel = discord.Object(id='') #YOUR DESIRED CHANNEL ID!!!!!!!!!!!!!!!!!!!!
-	alertMSG = '@everyone Lord Kazzak SPAWN - Blasted Lands ! :Kazzak:'
 
-	while True:
-		if counter <=2:
-			yield from client.send_message(channel, alertMSG)
-			yield from asyncio.sleep(1) # task runs every 60 seconds
-			counter += 1
-		else:
-			pass
-@client.async_event
+async def discordReport():
+	await client.wait_until_ready()
+	counter = 0
+	channel = client.get_channel() #place desired channel ID here as an integer
+
+	while counter <=2:
+		await channel.send('@everyone Kazzak SPAWN - Blasted Lands !')
+		counter += 1
+
+# @client.async_event
 def on_ready():
 	print('Logged in as')
 	print(client.user.name)
@@ -24,4 +20,4 @@ def on_ready():
 	print('------')
 
 client.loop.create_task(discordReport())
-client.run('')# YOUR BOT'S TOKEN HERE
+client.run('') #place discord bot's token here
