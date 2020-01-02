e261e27d02c62f1bffb2a2150928af543f1f2796
LucurisClassic <54478419+LucurisClassic@users.noreply.github.com>
2020-01-02 15:19:37 -0800

    Update Scout.py

--- Scout.py
+++ Scout.py
@@ -3,27 +3,79 @@ import sys
 import time
 import datetime
 import subprocess
-screenshotsfolder = 'C:\\Program Files (x86)\\World of Warcraft\\_classic_\\Screenshots'
-def eventTrigger(unixtimestamp,filename):
+import asyncio
+import discord
+import ffmpeg
+# screenshotsfolder = 'C:\\Program Files (x86)\\World of Warcraft\\_classic_\\Screenshots'
+screenshotsfolder= '/Users/tim/Desktop/scannerTest'
+client = discord.Client()
+
+async def eventTrigger(unixtimestamp,filename,voicechannel):
 	print ("Detected file %s @ %s" % (os.path.join(screenshotsfolder,"WoWScrnShot_"+filename+".jpg"), unixtimestamp))
 	if sys.argv[1] == '-Hinterlands':
-		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Hinterlands.py'
+		msg='@everyone Emerald Dragon SPAWN - Hinterlands ! <:emeraldDragons:644964005823119370>'
+		success = await discordReport(msg)
+		voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secret.mp3'))
+		if success == True:
+			time.sleep(120)
+			sys.exit(0)
 	elif sys.argv[1] == '-Ashenvale':
-		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Ashenvale.py'
+		msg='@everyone Emerald Dragon SPAWN - Ashenvale ! <:emeraldDragons:644964005823119370>'
+		success = await discordReport(msg)
+		voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secret.mp3'))
+		if success == True:
+			time.sleep(120)
+			sys.exit(0)
 	elif sys.argv[1] == '-Feralas':
-		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Feralas.py'
+		msg='@everyone Emerald Dragon SPAWN - Feralas ! <:emeraldDragons:644964005823119370>'
+		success = await discordReport(msg)
+		voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secret.mp3'))
+		if success == True:
+			time.sleep(120)
+			sys.exit(0)
 	elif sys.argv[1] == '-Duskwood':
-		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Duskwood.py'
+		msg='@everyone Emerald Dragon SPAWN - Duskwood ! <:emeraldDragons:644964005823119370>'
+		success = await discordReport(msg)
+		voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secret.mp3'))
+		if success == True:
+			time.sleep(120)
+			sys.exit(0)
 	elif sys.argv[1] == '-Kazzak':
-		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Kazzak.py'
+		msg='@everyone Kazzak SPAWN - Blasted Lands ! <:kazzak:644626880816873501>'
+		success = await discordReport(msg)
+		voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secret.mp3'))
+		if success == True:
+			time.sleep(120)
+			sys.exit(0)
 	elif sys.argv[1] == '-Azuregos':
-		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\Azuregos.py'
+		msg='@everyone Azuregos SPAWN - Azshara ! <:azuregos:644626896377872436>'
+		success = await discordReport(msg)
+		voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secret.mp3'))
+		if success == True:
+			time.sleep(120)
+			sys.exit(0)
 	else:
-		filePicker = 'C:\\Users\\lucuris\\Desktop\\NPCscan_Alert\\noZoneSpecified.py' 	
-	cmd = ['python3',filePicker]
-	subprocess.Popen(cmd)
+		msg='@everyone Something SPAWNED - My owner fucked up and did not specify where!'
+		success = await discordReport(msg)
+		voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secret.mp3'))
+		if success == True:
+			time.sleep(120)
+			sys.exit(0)
 
-def activeScanner():
+async def discordReport(msg):
+	counter = 0
+	coalition = client.get_channel(349500178971099136) #not actually coalition 
+	# tempest = client.get_channel(349500178971099136) #not actually tempest 
+	if msg!=None:
+		while counter <=2:
+			await coalition.send(msg)
+			# await tempest.send(msg)
+			counter += 1
+	return True
+
+async def activeScanner():
+	voicechannel=client.get_channel(126901181615308801)
+	vc = await voicechannel.connect(timeout=1.0, reconnect=True)
 	first_ran_time = time.time()
 	data = {}
 	try:
@@ -34,8 +86,8 @@ def activeScanner():
 					fndt = time.mktime(datetime.datetime.strptime(fndtstr, "%m%d%y_%H%M%S").timetuple())
 					if fndt not in data.keys():
 						if fndt > first_ran_time:
-							data[fndt]=fndtstr				 
-							msg = eventTrigger(fndt,fndtstr)
+							data[fndt]=fndtstr
+							msg = await eventTrigger(fndt,fndtstr,vc)
 						else:
 							#not from this session, ignore
 							pass
@@ -44,7 +96,17 @@ def activeScanner():
 						pass
 	except KeyboardInterrupt:
 		pass
-def main():
-	activeScanner()
-if __name__ == '__main__':
-	main()
+
+@client.event
+async def on_ready():
+	for guild in client.guilds:
+			if guild.name == GUILD:
+				break
+	print(f'{client.user} is connected to the following guild:\n')
+	print(f'{guild.name}(id: {guild.id})')
+	msg=None
+	await discordReport(msg)
+	await activeScanner()
+
+GUILD=126901181615308800
+client.run('NjE0OTYyMjY2MDIxODIyNDY1.Xg4z-Q.bzM1DapCv9MS3DvGBW27DC4rL7g')
