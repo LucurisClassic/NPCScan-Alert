ac6c4462ed09b038629f19e9fca82c34de2a897f
LucurisClassic <54478419+LucurisClassic@users.noreply.github.com>
2020-01-04 10:51:36 -0800

    Update Scout.py
    No longer absolute shit. Only partially shit.

--- Scout.py
+++ Scout.py
@@ -2,125 +2,68 @@ import os
 import sys
 import time
 import datetime
-import subprocess
 import asyncio
 import discord
 import ffmpeg
-# screenshotsfolder = 'C:\\Program Files (x86)\\World of Warcraft\\_classic_\\Screenshots'
-screenshotsfolder= '/Users/tim/Desktop/scannerTest'
-client = discord.Client()
 
-async def eventTrigger(unixtimestamp,filename,voicechannel):
-	print ("Detected file %s @ %s" % (os.path.join(screenshotsfolder,"WoWScrnShot_"+filename+".jpg"), unixtimestamp))
-	if sys.argv[1] == '-Hinterlands':
-		msg='@everyone Emerald Dragon SPAWN - Hinterlands ! <:emeraldDragons:644964005823119370>'
-		success = await discordReport(msg)
-		while True:
-			voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secretold.mp3'))
-			if success == True:
-				time.sleep(50)
-				await disconnect()
-				sys.exit(0)
-	elif sys.argv[1] == '-Ashenvale':
-		msg='@everyone Emerald Dragon SPAWN - Ashenvale ! <:emeraldDragons:644964005823119370>'
-		success = await discordReport(msg)
-		while True:
-			voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secretold.mp3'))
-			if success == True:
-				time.sleep(50)
-				await disconnect()
-				sys.exit(0)
-	elif sys.argv[1] == '-Feralas':
-		msg='@everyone Emerald Dragon SPAWN - Feralas ! <:emeraldDragons:644964005823119370>'
-		success = await discordReport(msg)
-		while True:
-			voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secretold.mp3'))
-			if success == True:
-				time.sleep(50)
-				await disconnect()
-				sys.exit(0)
-	elif sys.argv[1] == '-Duskwood':
-		msg='@everyone Emerald Dragon SPAWN - Duskwood ! <:emeraldDragons:644964005823119370>'
-		success = await discordReport(msg)
-		while True:
-			voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secretold.mp3'))
-			if success == True:
-				time.sleep(50)
-				await disconnect()
-				sys.exit(0)
-	elif sys.argv[1] == '-Kazzak':
-		msg='@everyone Kazzak SPAWN - Blasted Lands ! <:kazzak:644626880816873501>'
-		success = await discordReport(msg)
-		while True:
-			voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secretold.mp3'))
-			if success == True:
-				time.sleep(50)
-				await disconnect()
-				sys.exit(0)
-	elif sys.argv[1] == '-Azuregos':
-		msg='@everyone Azuregos SPAWN - Azshara ! <:azuregos:644626896377872436>'
-		success = await discordReport(msg)
-		while True:
-			voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secretold.mp3'))
-			if success == True:
-				time.sleep(50)
-				await disconnect()
-				sys.exit(0)
-	else:
-		msg='@everyone Something SPAWNED - My owner fucked up and did not specify where!'
-		success = await discordReport(msg)
-		while True:
-			voicechannel.play(discord.FFmpegPCMAudio('/Users/tim/Downloads/key_secretold.mp3'))
-			if success == True:
-				time.sleep(50)
-				await disconnect()
-				sys.exit(0)
+class MyClient(discord.Client):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.bg_task = self.loop.create_task(self.my_background_task())
 
-async def discordReport(msg):
-	counter = 0
-	coalition = client.get_channel(349500178971099136) #not actually coalition 
-	# tempest = client.get_channel(349500178971099136) #not actually tempest 
-	if msg!=None:
-		while counter <=2:
-			await coalition.send(msg)
-			# await tempest.send(msg)
-			counter += 1
-	return True
+	async def on_ready(self):
+		print('Logged in as')
+		print(self.user.name)
+		print(self.user.id)
+		print('------')
 
-async def activeScanner():
-	voicechannel=client.get_channel(126901181615308801)
-	vc = await voicechannel.connect(timeout=1.0, reconnect=True)
-	first_ran_time = time.time()
-	data = {}
-	try:
-		while True:
-			for file in os.listdir(screenshotsfolder):
-				if file.startswith("WoWScrnShot_") and file.endswith(".jpg"):
-					fndtstr =  file.replace("WoWScrnShot_",'').replace(".jpg",'')
-					fndt = time.mktime(datetime.datetime.strptime(fndtstr, "%m%d%y_%H%M%S").timetuple())
-					if fndt not in data.keys():
-						if fndt > first_ran_time:
-							data[fndt]=fndtstr
-							msg = await eventTrigger(fndt,fndtstr,vc)
-						else:
-							#not from this session, ignore
-							pass
-					else:
-						#already know about it, ignore
-						pass
-	except KeyboardInterrupt:
-		pass
+	async def my_background_task(self):
+		await self.wait_until_ready()
+		async def eventTrigger(self,unixtimestamp,filename,voicechannel):
+			screenshotsfolder = 'C:\\Program Files (x86)\\World of Warcraft\\_classic_\\Screenshots'
+			print ("Detected file %s @ %s" % (os.path.join(screenshotsfolder,"WoWScrnShot_"+filename+".jpg"), unixtimestamp))
+			msg='@everyone %s SPAWN ! <:emeraldDragons:644964005823119370> <:kazzak:644626880816873501> <:azuregos:644626896377872436>' % sys.argv[1]
+			success = await discordReport(self,msg)
+			success=True
+			voicechannel.play(discord.FFmpegPCMAudio('C:\\Users\\timot\\Desktop\\newScan_Alert\\Brain_Power.mp3'))
+			time.sleep(300)
+			await voicechannel.disconnect()
+			sys.exit()
+			
+		async def discordReport(self,msg):
+			counter = 0
+			coalition = client.get_channel(644260570744487946) 
+			# whatheflux = client.get_channel(349500178971099136)
+			while counter <=2:
+				await coalition.send(msg)
+				# await whatheflux.send(msg)
+				counter += 1
+			return True
 
-@client.event
-async def on_ready():
-	for guild in client.guilds:
-			if guild.name == GUILD:
-				break
-	print(f'{client.user} is connected to the following guild:\n')
-	print(f'{guild.name}(id: {guild.id})')
-	msg=None
-	await discordReport(msg)
-	await activeScanner()
+		async def activeScanner(self,voicechannel):
+			vc = await voicechannel.connect(reconnect=True)
+			screenshotsfolder = 'C:\\Program Files (x86)\\World of Warcraft\\_classic_\\Screenshots'			
+			first_ran_time = time.time()
+			data = {}
+			try:
+				while True:
+					for file in os.listdir(screenshotsfolder):
+						if file.startswith("WoWScrnShot_") and file.endswith(".jpg"):
+							fndtstr =  file.replace("WoWScrnShot_",'').replace(".jpg",'')
+							fndt = time.mktime(datetime.datetime.strptime(fndtstr, "%m%d%y_%H%M%S").timetuple())
+							if fndt not in data.keys():
+								if fndt > first_ran_time:
+									data[fndt]=fndtstr
+									msg = await eventTrigger(self,fndt,fndtstr,vc)
+								else:	
+									pass #not from this session, ignore
+							else:
+								pass #already know about it, ignore
+			except KeyboardInterrupt:
+				pass
+		voicechannel=client.get_channel(661011695732523049)  # COALITION DISCORD
+		# voicechannel=client.get_channel(126901181615308801)# WHATHEFLUX DISCORD
+		await activeScanner(self,voicechannel)
 
-GUILD=126901181615308800
-client.run('NjE0OTYyMjY2MDIxODIyNDY1.Xg4z-Q.bzM1DapCv9MS3DvGBW27DC4rL7g')
+client = MyClient()
+client.run('MzQ5NDgzMTQ4NTE5NjY5NzYy.Xg6QCw.CCkE1f0zRqoBLQYZgrj3mD8RkAA')
